b8d4e7622d72d27a53214be39357a852af788a0f
AvramArtem <artemhop165@gmail.com>
2023-09-30 22:31:06 +0300

    Disabled function

--- HM №8/index.js
+++ HM №8/index.js
@@ -18,19 +18,38 @@ function allBlack() {
     }
 }
 
+function disabled(){
+    
+    if(ul.childElementCount === 0){
+        firstInput.setAttribute("disabled", "true");
+        lastInput.setAttribute("disabled", "true");
+        nextInput.setAttribute("disabled", "true");
+        previousInput.setAttribute("disabled", "true");
+    }else{
+        firstInput.removeAttribute("disabled");
+        lastInput.removeAttribute("disabled");
+        nextInput.removeAttribute("disabled");
+        previousInput.removeAttribute("disabled");
+        
+    }
+}
+
 firstInput.onclick = function(){
     allBlack();
+    disabled();
     const firstElement = ul.firstElementChild.classList.add("select");
 }
 
 lastInput.onclick = function(){
     allBlack();
+    disabled();
     const lastElemenet = ul.lastElementChild.classList.add("select");
 }
 
 nextInput.onclick = function(){
     let selected = document.querySelector(".select");
     allBlack();
+    disabled();
     if(selected.nextElementSibling){
         selected.nextElementSibling.classList.add("select");
     }else{
@@ -41,6 +60,7 @@ nextInput.onclick = function(){
 previousInput.onclick = function(){
     let selected = document.querySelector(".select");
     allBlack();
+    disabled();
     if(selected.previousElementSibling){
         selected.previousElementSibling.classList.add("select");
     }else{
@@ -49,17 +69,20 @@ previousInput.onclick = function(){
 }
 
 addToTheEndInput.onclick = function(){
+    disabled();
     let addToTheEndElement = document.createElement("li");
     addToTheEndElement.textContent = "New Item";
     ul.append(addToTheEndElement);
 }
 deleteAtTheBeginningInput.onclick = function(){
+    disabled();
     let firstElementDelete = ul.firstElementChild;
     if(firstElementDelete){
         ul.removeChild(firstElementDelete);
     }
 }
 deleteAtTheEndInput.onclick = function(){
+    disabled();
     let lastElementDelete = ul.lastElementChild;
     if(lastElementDelete){
         ul.removeChild(lastElementDelete);
@@ -67,6 +90,7 @@ deleteAtTheEndInput.onclick = function(){
 }
 
 deleteAtTheCenterInput.onclick = function(){
+    disabled();
     if(ul.childElementCount <= 2){
         alert("Тут немає центральних елементів для видалення!")
     }
@@ -83,6 +107,7 @@ deleteAtTheCenterInput.onclick = function(){
 }
 
 addToTheBeginningInput.onclick = function(){
+    disabled();
     let addToTheBeginningElement = document.createElement("li");
     addToTheBeginningElement.textContent = "New Item";
     ul.prepend(addToTheBeginningElement);
